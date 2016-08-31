66f44f473079954c0aada9db2b82f622b53bf105
Kim Desrosiers <kimdesro@gmail.com>
2016-08-31 15:42:44 -0400

    First FT now uses unittest

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,23 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
