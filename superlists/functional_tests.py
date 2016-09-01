16d5c52a48c05bb209a0d810090314cbd1188de0
Kim Desrosiers <kimdesro@gmail.com>
2016-09-01 14:39:47 -0400

    Redirect after POST, and show all items in template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -42,7 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
         input_box = self.browser.find_element_by_id('id_new_item')
-        input_box.send_keys('use peacock feathers to make a fly')
+        input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
