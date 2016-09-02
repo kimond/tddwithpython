eb59680fa86b9825dfc2a859518c534a9c7b0fb9
Kim Desrosiers <kimdesro@gmail.com>
2016-09-01 21:23:20 -0400

    Add new url to handle user lists. Refactor lists tests. Add new template.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -36,22 +36,28 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEquals(response.status_code, 302)
-        self.assertEquals(response['location'], '/')
+        self.assertEquals(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEquals(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_list_items(self):
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
 
 class ItemModelTest(TestCase):
