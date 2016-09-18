05d9caf99ed1b1ccf43dc45a83d49cb1d05928ff
Kim Desrosiers <kimdesro@gmail.com>
2016-09-17 22:43:18 -0400

    Handle logout

--- superlists/accounts/urls.py
+++ superlists/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
 
 from .views import persona_login
 
 urlpatterns = [
-    url(r'login$', persona_login, name='persona_login'),
+    url(r'^login$', persona_login, name='persona_login'),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
